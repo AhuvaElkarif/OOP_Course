4f13d015b28bec16ba1e7f651010d78d4234e9b6
Ahuva <ahuvael02@gmail.com>
2024-11-29 00:35:37 +0200

    add lesson2

---
+++ Lesson1/Lesson1/OperationTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class OperationTable
+    {
+        public int input;
+        public int [,] results = new int[10,10];
+
+        public OperationTable(int input)
+        {
+            this.input = input;
+        }
+
+        public void Calculate()
+        {
+            for (int i = 1; i <= results.GetLength(0); i++)
+            {
+                for (int j = 1; j <= results.GetLength(1); i++)
+                {
+                    results[i, j] = i * j;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            for (int i = 0; i < results.GetLength(0); i++)
+            {
+                for (int j = 0; j < results.GetLength(1); i++)
+                {
+                    res += results[i,j].ToString() + " ";    
+                }
+                res += "\n";
+            }
+            return res;
+        }
+    }
+}
---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    struct PointStruct
+    {
+        public int x;
+        public int y;
+    }
+    public static void MeasureStructMemory()
+    {
+        long before = GC.GetAllocatedBytesForCurrentThread();
+        PointStruct pointStruct = new PointStruct();
+        long after = GC.GetAllocatedBytesForCurrentThread();
+        Console.WriteLine($"MeasureStructMemory: Allocated Memory={after - before}");
+    }
+
+    public static void MeasureStringMemory()
+    {
+        long before = GC.GetAllocatedBytesForCurrentThread();
+        string s = new string("hello world");
+        long after = GC.GetAllocatedBytesForCurrentThread();
+        Console.WriteLine($"MeasureStringMemory: Allocated Memory={after - before}");
+    }
+
+    public static void MeasureMemory()
+    {
+        long before = GC.GetAllocatedBytesForCurrentThread();
+        long after = GC.GetAllocatedBytesForCurrentThread();
+        Console.WriteLine($"MeasureStringMemory: Allocated Memory={after - before}");
+    }
+
+    public static string MeasureStringMemory2()
+    {
+        long before = GC.GetAllocatedBytesForCurrentThread();
+
+        string str = "";
+        for (int i = 1; i <= 100; i++)
+        {
+            str += i + " ";
+        }
+        long after = GC.GetAllocatedBytesForCurrentThread();
+        Console.WriteLine($"MeasureStringMemory: Allocated Memory={after - before}");
+        return str;
+    }
+
+    public static StringBuilder MeasureStringBuilderMemory()
+    {
+        long before = GC.GetAllocatedBytesForCurrentThread();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= 100; i++)
+        {
+            stringBuilder.Append(i + " ");
+        }
+        long after = GC.GetAllocatedBytesForCurrentThread();
+        Console.WriteLine($"MeasureStringMemory: Allocated Memory={after - before}");
+        return stringBuilder;
+    }
+
+    public static void TestGC()
+    {
+        object obj = new object();
+        Console.WriteLine($"Generation before GC {GC.GetGeneration(obj)}");
+        GC.Collect(0);
+        Console.WriteLine($"Generation after GC of Generation 0: {GC.GetGeneration(obj)}");
+    }
+
+    static void Main(string[] args)
+    {
+        #region Lesson2
+        // Ex 1
+        MeasureStructMemory();
+        // Ex 2
+        MeasureStringMemory();
+        // Ex 3
+        string str = MeasureStringMemory2();
+        StringBuilder stringBuilder = MeasureStringBuilderMemory();
+        // Ex 4
+        TestGC();
+        #endregion
+    }
+}
+
